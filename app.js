2264650a9f1f9c6a735527cc1422a152c6bf13ac
MarcosAdrianoGuedes <marcosadri55@hotmail.com>
2023-10-03 13:50:08 -0300

    video teste

---
+++ app.js
@@ -0,0 +1,53 @@
+// Lógica JavaScript aqui
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function toggleZoom(element) {
+    element.classList.toggle('zoom-in');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.section-button');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', function () {
+            const sectionId = button.getAttribute('data-section');
+            scrollToSection(sectionId);
+        });
+    });
+
+    const scrollButton = document.querySelector('.scroll-to-top');
+    scrollButton.addEventListener('click', scrollToTop);
+
+    const containers = document.querySelectorAll('.container');
+    containers.forEach((container) => {
+        container.addEventListener('click', function () {
+            toggleZoom(this);
+        });
+    });
+});
+
+window.addEventListener('scroll', toggleScrollToTopButton);
+
+function toggleScrollToTopButton() {
+    const scrollButton = document.getElementById('scrollToTopButton');
+    if (window.pageYOffset > window.innerHeight) {
+        scrollButton.style.display = 'block';
+    } else {
+        scrollButton.style.display = 'none';
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
